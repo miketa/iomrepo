7b40b2f2026a374c6a881f0485005efdf5155356
miketa <miketa.arvaniti@gmail.com>
2014-04-22 11:17:37 +0300

    first commit

---
+++ sites/all/themes/iomcountryv2/templates/html.tpl.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * @file
+ * Returns the HTML for the basic html structure of a single Drupal page.
+ *
+ * Complete documentation for this file is available online.
+ * @see https://drupal.org/node/1728208
+ */
+?><!DOCTYPE html>
+<!--[if IEMobile 7]><html class="iem7" <?php print $html_attributes; ?>><![endif]-->
+<!--[if lte IE 6]><html class="lt-ie9 lt-ie8 lt-ie7" <?php print $html_attributes; ?>><![endif]-->
+<!--[if (IE 7)&(!IEMobile)]><html class="lt-ie9 lt-ie8" <?php print $html_attributes; ?>><![endif]-->
+<!--[if IE 8]><html class="lt-ie9" <?php print $html_attributes; ?>><![endif]-->
+<!--[if (gte IE 9)|(gt IEMobile 7)]><!--><html <?php print $html_attributes . $rdf_namespaces; ?>><!--<![endif]-->
+
+<head>
+  <?php print $head; ?>
+  <title><?php print $head_title; ?></title>
+
+  <?php if ($default_mobile_metatags): ?>
+    <meta name="MobileOptimized" content="width">
+    <meta name="HandheldFriendly" content="true">
+    <meta name="viewport" content="width=device-width">
+  <?php endif; ?>
+  <meta http-equiv="cleartype" content="on">
+
+  <?php print $styles; ?>
+  <?php print $scripts; ?>
+  <?php if ($add_html5_shim and !$add_respond_js): ?>
+    <!--[if lt IE 9]>
+    <script src="<?php print $base_path . $path_to_zen; ?>/js/html5.js"></script>
+    <![endif]-->
+  <?php elseif ($add_html5_shim and $add_respond_js): ?>
+    <!--[if lt IE 9]>
+    <script src="<?php print $base_path . $path_to_zen; ?>/js/html5-respond.js"></script>
+    <![endif]-->
+  <?php elseif ($add_respond_js): ?>
+    <!--[if lt IE 9]>
+    <script src="<?php print $base_path . $path_to_zen; ?>/js/respond.js"></script>
+    <![endif]-->
+  <?php endif; ?>
+</head>
+<body class="<?php print $classes; ?>" <?php print $attributes;?>>
+  <?php if ($skip_link_text && $skip_link_anchor): ?>
+    <p id="skip-link">
+      <a href="#<?php print $skip_link_anchor; ?>" class="element-invisible element-focusable"><?php print $skip_link_text; ?></a>
+    </p>
+  <?php endif; ?>
+	<div id="wrapper">
+		<div class="stay-connected">
+			<span>IOM Global Corner:</span>
+			<a href="http://www.iom.int"><span class="home-icon">Home</span></a>
+			<a href="http://www.facebook.com/pages/International-Organization-for-Migration/161303029020?ref=mf" target="_blank"><span class="fb">facebook</span></a>
+			<a href="http://twitter.com/IOM_news" target="_blank"><span class="tw">twitter</span></a>
+			<a href="http://www.flickr.com/photos/iom-migration/" target="_blank"><span class="fl">flickr</span></a>
+			<a href="http://www.youtube.com/iommigration" target="_blank"><span class="yb">youtube</span></a>
+			<a href="http://contacts.iom.int/user/register" target="_blank"><span class="subscribe">Subscribe</span></a>
+		</div>
+	</div>
+  <?php print $page_top; ?>
+  <?php print $page; ?>
+  <?php print $page_bottom; ?>
+</body>
+</html>
